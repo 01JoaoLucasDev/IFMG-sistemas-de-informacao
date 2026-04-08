c8136aae2968772f85090b6e050a715b6026b3f2
Joao Lucas <joaolucascosta32@gmail.com>
2026-04-08 10:05:04 -0300

    (feature)exercicio_estrutura_sequencial

---
+++ PRIMEIRO_PERIODO/eestrutura_sequencial/Joao1.java
@@ -0,0 +1,36 @@
+// IFMG - Campus Sabará
+// EXERCICIO 1 - João Lucas da Costa Bernardo
+
+import java.util.Scanner;
+public class Joao1 {
+    public static void main(String[] args) {
+        
+
+    Scanner sc = new Scanner(System.in);
+
+   System.out.println("Informe a sua idade: ");
+   int idade = sc.nextInt();
+
+   if (idade >= 5 && idade <= 7) {
+        System.out.println("categoria: infantil");
+
+   } else if (idade >= 8 && idade <= 10 ) {
+        System.out.println("categoria: juvenil");
+
+    } else if (idade >= 11 && idade <= 15) {
+            System.out.println("categoria: adolescente");
+
+    } else if (idade >= 16 && idade <= 40 ) {
+                System.out.println("categoria: Adulto");
+
+    } else if (idade > 40) {
+            System.out.println("categoria: senior");
+
+        
+    }
+    
+   }
+    
+}
+
+
