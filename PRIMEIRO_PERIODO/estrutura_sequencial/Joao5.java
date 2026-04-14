e13f3927dfe01fc26063c33e6ea15a2fa7865ccb
Joao Lucas <joaolucascosta32@gmail.com>
2026-04-14 09:22:49 -0300

    (feature)correcao-estrutura-sequencial

--- PRIMEIRO_PERIODO/estrutura_sequencial/Joao5.java
+++ PRIMEIRO_PERIODO/estrutura_sequencial/Joao5.java
@@ -6,8 +6,11 @@ public class Joao5 {
     public static void main (String[] args) {
      System.out.println("Digite uma letra:") ;
      Scanner sc = new Scanner(System.in);
-        String letra = sc.nextLine();
-        if (letra == "a" || letra == "A" || letra == "e" || letra == "E" || letra == "i" || letra == "I" || letra == "o" || letra == "O" || letra == "u" || letra == "U") {
+
+        String letra = sc.nextLine().charAt(0);
+        letra = Character.toUpperCase();
+        
+        if (letra.equals("a") || Letra.equals("e") || letra.equals(i) || letra.equals(0) || letra.equals(u)) {
             System.out.println("A letra é uma vogal");
         } else {
             System.out.println("A letra é uma consoante");
