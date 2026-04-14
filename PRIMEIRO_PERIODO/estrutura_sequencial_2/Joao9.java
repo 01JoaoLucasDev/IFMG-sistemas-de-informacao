16b4419216054d73acb1af5acd7e9aa86b720b2c
Joao Lucas <joaolucascosta32@gmail.com>
2026-04-14 10:23:09 -0300

    (feature)realizando-novas-atividades

---
+++ PRIMEIRO_PERIODO/estrutura_sequencial_2/Joao9.java
@@ -0,0 +1,20 @@
+package estrutura_sequencial_2;
+import java.util.Scanner;
+
+public class Joao9 {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite o primeiro numero: ");
+    int a = sc.nextInt();
+
+    System.out.println("Digite outro n numero: ");
+    int b = sc.nextInt();
+
+    System.out.println("Digite o ultimo numero: ");
+    int c = sc.nextInt()
+    
+}
+}
