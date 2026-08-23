b847211d9062137ea102b5734a046c517540e8ff
João Lucas <joaolucascosta32@gmail.com>
2026-08-23 19:09:12 -0300

    (feature)melhorando chaveamento de pastas

---
+++ PRIMEIRO_PERIODO/PROGRAMAÇÃO1/exercicios_basicos/Joao9.java
@@ -0,0 +1,11 @@
+package exercicios_basicos;
+import java.util.Scanner;
+
+// IFMG - Campus Sabará
+// EXERCICIO 9 - João Lucas da Costa Bernardo
+public class Joao9 {public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+
+}
+}
